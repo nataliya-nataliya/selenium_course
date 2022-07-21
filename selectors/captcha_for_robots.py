3f7d9a5d79c5e66011455ad0572cf60e3e75a4a3
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-07-21 21:48:43 +0300

    big footer

---
+++ selectors/captcha_for_robots.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://SunInJuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    value = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    number = value.text
+    res = calc(number)
+    button = browser.find_element(By.TAG_NAME, "button")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+
+    in1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    in1.send_keys(res)
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    in2 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    in2.click()
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    in3 = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    in3.click()
+    button.click()
+finally:
+    time.sleep(10)
+    browser.quit()
+
+
+
